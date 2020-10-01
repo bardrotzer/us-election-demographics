510c04945c88311baee9167b1d058fb1f820d2f2
Bård Røtzer <bard@Bards-MacBook-Pro.local>
2020-10-01 11:35:05 +0300

    Add libraries and build instructions

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        full10: '10vh',
+        full25: '25vh',
+        full50: '50vh',
+        full75: '75vh'
+      },
+      colors: {
+        link: '#0babc3',
+      }
+    },
   },
   variants: {},
   plugins: [],
